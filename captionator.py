06f07f6ba889456e01161236807a124fbbc5d66c
Patrick Leiser <patrick27leiser@yahoo.com>
2022-04-17 05:09:49 +0100

    add start of twilio integration

--- captionator.py
+++ captionator.py
@@ -13,6 +13,9 @@ from scipy import signal
 from RPLCD.gpio import CharLCD
 from RPi import GPIO
 
+from twilio.rest import Client
+
+
 lcd = CharLCD(pin_rs=15, pin_rw=18, pin_e=16, pins_data=[21, 22, 23, 24],
               numbering_mode=GPIO.BOARD,
               cols=24, rows=2)
