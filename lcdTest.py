066da00f0b916b325933cb39710ab7d65c98a1fb
Patrick Leiser <patrick27leiser@yahoo.com>
2022-04-17 03:50:24 +0100

    specifiy lcd sizr

--- lcdTest.py
+++ lcdTest.py
@@ -3,8 +3,13 @@ from RPi import GPIO
 
 
 lcd = CharLCD(pin_rs=15, pin_rw=18, pin_e=16, pins_data=[21, 22, 23, 24],
-              numbering_mode=GPIO.BOARD)
+              numbering_mode=GPIO.BOARD,
+              cols=24, rows=2)
+
+lcd.clear()
+
+lcd.cursor_pos = (0, 0)
 
 lcd.write_string('Raspberry Pi HD44780')
-lcd.cursor_pos = (2, 0)
+lcd.cursor_pos = (1, 0)
 lcd.write_string('https://github.com/\n\rdbrgn/RPLCD')
